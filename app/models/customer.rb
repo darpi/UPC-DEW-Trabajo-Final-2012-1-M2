389164018f57ef95433ff10232001d82267f1c73
Dora Arpi <darpielena@hotmail.com>
2012-08-10 19:35:33 -0500

    Modificacion ruteo

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,4 @@
 class Customer < ActiveRecord::Base
 	has_many:patients
 	has_many:vaccines
-	  
-	def new do
-	   @customer = Customer.new
-	end 
-	
-	def create do
-	   @customer = Customer.new
-	end 
 end
